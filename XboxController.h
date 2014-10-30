2f07ddde0b870b6971720b900dea6ba201234c69
Sam <samhayhurst@gmail.com>
2014-10-30 20:08:34 +0000

    XboxController class and header file added

---
+++ XboxController.h
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// (C) Sam Hayhurst 2014
+//
+// Xbox controller using xinput.h
+//
+
+
+#ifndef XBOXCONTROLLER_INCLUDED
+#define XBOXCONTROLLER_INCLUDED
+
+#include <Xinput.h>
+
+class XboxController {
+
+
+
+};
+
+#endif 
